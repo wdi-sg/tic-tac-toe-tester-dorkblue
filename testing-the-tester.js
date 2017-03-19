8f120de228c47717b9771a1540058e245db63288
Tom Lee <d.dorkblue@gmail.com>
2017-03-19 18:47:15 +0800

    added isGameOver() to beginning of playTurn(). made some changes to incorporate some of the exercise conditions i missed. game not working now

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,41 +1,48 @@
 var array = []
 var turn = 0
+var status = null
 
 function restart () {
   array = [ null, null, null,
     null, null, null,
     null, null, null]
-  return whoWon()
+  turn = 0
 }
 
 function isGameOver () {
+  if (whoWon() > 0) {
+    return true
+  }
   return false
 }
+
 function whoWon () {
+  console.log(array)
   if (turn === 0) return 0
   if (turn > 8) {
-    turn = 0
     return 3
   } else if (turn > 4 && (turn % 2 !== 0)) {
-    turn = 0
     return 1
   } else if (turn > 5 && (turn % 2 === 0)) {
-    turn = 0
     return 2
+  } else {
+
   }
 }
 
 // this works
 function playTurn (move) {
-  if (turn % 2 !== 0) {
-    if (array[move] !== null) return false
-    turn++
-    array[move] = 1
-    return true
-  } else {
-    if (array[move] !== null) return false
-    turn++
-    array[move] = 2
-    return true
+  if (isGameOver() === false) {
+    if (turn % 2 === 0) {
+      if (array[move] !== null) return false
+      turn++
+      array[move] = 1
+      return true
+    } else {
+      if (array[move] !== null) return false
+      turn++
+      array[move] = 2
+      return true
+    }
   }
 }
