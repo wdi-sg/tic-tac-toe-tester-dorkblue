52c9806e3de8ebb65b9899dff2c9f22aff67d3e3
Tom Lee <d.dorkblue@gmail.com>
2017-03-19 22:40:00 +0800

    changed game check logic

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -1,68 +1,75 @@
 var gameBoard = []
 var turn = 0
-// var winner = null
+var gameStatus = null
+var WinningCom = function (gameBoard) {
+  this.topRow = gameBoard[0] + gameBoard[1] + gameBoard[2]
+  this.midRow = gameBoard[3] + gameBoard[4] + gameBoard[5]
+  this.botRow = gameBoard[6] + gameBoard[7] + gameBoard[8]
+  this.leftCol = gameBoard[0] + gameBoard[3] + gameBoard[6]
+  this.midCol = gameBoard[1] + gameBoard[4] + gameBoard[7]
+  this.rightCol = gameBoard[2] + gameBoard[5] + gameBoard[8]
+  this.diagL2R = gameBoard[0] + gameBoard[4] + gameBoard[8]
+  this.diagR2L = gameBoard[2] + gameBoard[4] + gameBoard[6]
+}
 
 function restart () {
   gameBoard = [ null, null, null,
     null, null, null,
     null, null, null]
-  whoWon()
+  turn = 0
+  gameStatus = null
 }
 
 function isGameOver () {
-  return false
+  if (gameStatus === null || gameStatus === 'ongoing') {
+    return false
+  } else if (gameStatus === 'game ended') return true
 }
 
 function whoWon () {
-  var winner = null
-  var winningCom = {
-    topRow: gameBoard[0] + gameBoard[1] + gameBoard[2],
-    midRow: gameBoard[3] + gameBoard[4] + gameBoard[5],
-    botRow: gameBoard[6] + gameBoard[7] + gameBoard[8],
-    leftCol: gameBoard[0] + gameBoard[3] + gameBoard[6],
-    midCol: gameBoard[1] + gameBoard[4] + gameBoard[7],
-    rightCol: gameBoard[2] + gameBoard[5] + gameBoard[8],
-    diagL2R: gameBoard[0] + gameBoard[4] + gameBoard[8],
-    diagR2L: gameBoard[2] + gameBoard[4] + gameBoard[6]
-  }
-
-  if (turn !== 0) {
-    winner = 'player3'
-    for (var test in winningCom) {
-      if (winningCom[test] === 3) {
-        winner = 'player1'
-        break
-      } else if (winningCom[test] === -3) {
-        winner = 'player2'
-        break
-      }
-    }
-  }
-
-  if (winner === 'player1') {
-    turn = 0
-    return 1
-  } else if (winner === 'player2') {
-    turn = 0
-    return 2
-  } else if (winner === 'player3') {
-    turn = 0
-    return 3
-  } else {
+  if (gameStatus === null) {
     return 0
   }
+  var checkWin = new WinningCom(gameBoard)
+  // console.log(checkWin)
+  // var winningCom = {
+  //   topRow: gameBoard[0] + gameBoard[1] + gameBoard[2],
+  //   midRow: gameBoard[3] + gameBoard[4] + gameBoard[5],
+  //   botRow: gameBoard[6] + gameBoard[7] + gameBoard[8],
+  //   leftCol: gameBoard[0] + gameBoard[3] + gameBoard[6],
+  //   midCol: gameBoard[1] + gameBoard[4] + gameBoard[7],
+  //   rightCol: gameBoard[2] + gameBoard[5] + gameBoard[8],
+  //   diagL2R: gameBoard[0] + gameBoard[4] + gameBoard[8],
+  //   diagR2L: gameBoard[2] + gameBoard[4] + gameBoard[6]
+  // }
+  var winner = null
+  // console.log(gameBoard)
+
+  for (var check in checkWin) {
+    if (checkWin[check] === 3) {
+      gameStatus = 'game ended'
+      return 1
+    } else if (checkWin[check] === -3) {
+      gameStatus = 'game ended'
+      return 2
+    }
+  }
+  return 3
 }
 
 function playTurn (move) {
-  if (turn % 2 === 0) {
-    if (gameBoard[move] !== null) return false
-    turn++
-    gameBoard[move] = 1
-    return true
-  } else {
-    if (gameBoard[move] !== null) return false
-    turn++
-    gameBoard[move] = -1
-    return true
+  if (isGameOver() === false) {
+    if (turn % 2 === 0) {
+      if (gameBoard[move] !== null) return false
+      turn++
+      gameBoard[move] = 1
+      return gameStatus = 'ongoing', true
+    } else {
+      if (gameBoard[move] !== null) return false
+      turn++
+      gameBoard[move] = -1
+      return gameStatus = 'ongoing', true
+    }
   }
+  return false
 }
